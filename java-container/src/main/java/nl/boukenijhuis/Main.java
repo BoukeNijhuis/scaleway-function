c4ef6be4f3135005e97b52be1485ff4c14095b8f
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2025-09-24 16:47:40 +0200

    created a special health endpoint

--- java-container/src/main/java/nl/boukenijhuis/Main.java
+++ java-container/src/main/java/nl/boukenijhuis/Main.java
@@ -12,20 +12,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         // scaleway needs a way to check the health of the container
-        final HttpServer server = createHttpServer();
+        final HttpServer server = createHttpServerWithHealthEndpoint();
         server.start();
 
         Checker checker = new Checker();
         checker.checkSolarPanels();
 
         // stop the server (health endpoint)
-        server.stop(0);
+//        server.stop(0);
     }
 
     @NotNull
-    private static HttpServer createHttpServer() throws IOException {
+    private static HttpServer createHttpServerWithHealthEndpoint() throws IOException {
         final HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/", t -> {
+        server.createContext("/health", t -> {
             String response = "The solar-panel-checker is running";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
